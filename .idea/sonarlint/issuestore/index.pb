fef4f352db4853337e9aaa7c04c8e2898910c3e1
Elizeu Ramos <elizeu.nascimento@autbank.com.br>
2021-12-10 18:10:56 -0300

    dev - first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+n
+>src/main/java/br/com/pegasus/forum/ForumOnlineApplication.java,a\f\af00608c1026e92c8136d9dc44e5dbd222de74fe
---
+++ src/main/java/br/com/pegasus/forum/ForumOnlineApplication.java
@@ -0,0 +1,15 @@
+package br.com.pegasus.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class ForumOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumOnlineApplication.class, args);
+	}
+
+}
